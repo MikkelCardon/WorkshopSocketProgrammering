46963c7e8429fb2dfc4a809a0abe0f07cc8ff8e3
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-08-29 10:08:02 +0200

    opgave 3 med UDP DNS - lavet på Emil computer

---
+++ src/main/java/opgave03/customDNS/version_UDP/ClientTCP.java
@@ -0,0 +1,151 @@
+package opgave03.customDNS.version_UDP;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.Scanner;
+
+public class ClientTCP {
+    private static DataOutputStream outToServer;
+    private static BufferedReader inFromServer;
+
+    public static void main(String[] args) {
+        String inputMessage = initialConnection();
+//        connectToServer(inputMessage, 10_000);
+
+//        String responseFromDNS = requestDNS_TCP();
+        String responseFromDNS = requestDNS_UDP(inputMessage, 10_000);
+        System.out.println("responseFromDNS: " + responseFromDNS);
+        connectToServer(responseFromDNS, 10_001);
+
+        Thread writeThread = new Thread(() -> writeToServer());
+        Thread readThread = new Thread(() -> readFromServer());
+
+        writeThread.start();
+        readThread.start();
+    }
+
+    private static String initialConnection(){
+        Scanner initialConnection = new Scanner(System.in);
+        System.out.print("DNS Server IP Address: ");
+        return initialConnection.nextLine().trim();
+    }
+
+    private static String requestDNS_TCP(){
+        System.out.print("Request to DNS: ");
+        Scanner clientInput = new Scanner(System.in);
+        String replyFromDNSServer;
+
+        while (true) {
+            String commandToDNS = clientInput.nextLine();
+            String[] splitedCommandToDNS = commandToDNS.toLowerCase().split(" ");
+            try {
+                outToServer.writeBytes(commandToDNS + '\n');
+                replyFromDNSServer = inFromServer.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            if (splitedCommandToDNS[0].equals("connect")) {
+                break;
+            }
+            System.out.println(replyFromDNSServer);
+        }
+
+        return replyFromDNSServer;
+    }
+
+    private static String requestDNS_UDP(String ipAddress, int port){
+        try {
+            DatagramSocket clientSocket = new DatagramSocket();
+            System.out.print("Request to DNS: ");
+            Scanner inputFromClient = new Scanner(System.in);
+            String requestToDNS;
+            DatagramPacket packet;
+
+            byte[] sendToDNS;
+            byte[] receiveFromDNS;
+            String responseFromDNS;
+
+            while(true){
+                sendToDNS = new byte[1024];
+                receiveFromDNS = new byte[1024];
+
+                requestToDNS = inputFromClient.nextLine();
+                String[] splittedRequest = requestToDNS.toLowerCase().split(" ");
+                sendToDNS = requestToDNS.getBytes();
+
+                packet = new DatagramPacket(
+                        sendToDNS,
+                        sendToDNS.length,
+                        InetAddress.getByName(ipAddress),
+                        port
+                );
+                clientSocket.send(packet);
+
+                packet = new DatagramPacket(
+                        receiveFromDNS,
+                        receiveFromDNS.length
+                );
+                clientSocket.receive(packet);
+                responseFromDNS = new String(packet.getData()).trim();
+
+                if (splittedRequest[0].equals("connect")) {
+                    break;
+                }
+                System.out.println(responseFromDNS);
+            }
+            return responseFromDNS;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void connectToServer(String ipAddress, int port){
+        try {
+            Socket clientSocket = new Socket(ipAddress, port);
+            outToServer = new DataOutputStream(
+                    clientSocket.getOutputStream()
+            );
+            inFromServer = new BufferedReader(
+                    new InputStreamReader(
+                            clientSocket.getInputStream()
+                    )
+            );
+        } catch (UnknownHostException e){
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void writeToServer(){
+        Scanner messageScanner = new Scanner(System.in);
+        String messageToServer;
+        while (true) {
+            try {
+                System.out.print("Input message: ");
+                messageToServer = messageScanner.nextLine();
+                outToServer.writeBytes(messageToServer + '\n');
+                System.out.println("Send: " + messageToServer);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static void readFromServer(){
+        String messageFromServer;
+        while (true) {
+            try {
+                messageFromServer = inFromServer.readLine();
+                System.out.println("Received: " + messageFromServer);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/opgave03/customDNS/version_UDP/ServerTCP.java
@@ -0,0 +1,68 @@
+package opgave03.customDNS.version_UDP;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ServerTCP {
+    private static BufferedReader inFromClient;
+    private static DataOutputStream outToClient;
+
+    public static void main(String[] args) {
+        try (ServerSocket welcomeSocket = new ServerSocket(10_001)) {
+            Socket connectionSocket = welcomeSocket.accept();
+            System.out.println("[Client connected to Server from IP: " + connectionSocket.getInetAddress() + "]");
+
+            inFromClient = new BufferedReader(
+                    new InputStreamReader(
+                            connectionSocket.getInputStream()
+                    )
+            );
+            outToClient = new DataOutputStream(
+                    connectionSocket.getOutputStream()
+            );
+            Thread readThread = new Thread(() -> readFromClient());
+            Thread writeThread = new Thread(() -> writeToClient());
+
+            readThread.start();
+            writeThread.start();
+        } catch (UnknownHostException e){
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void readFromClient(){
+        String messageFromClient;
+        while (true) {
+            try {
+                messageFromClient = inFromClient.readLine();
+                System.out.println("Received: " + messageFromClient);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static void writeToClient(){
+        Scanner scanner = new Scanner(System.in);
+        String messageToClient;
+        while (true) {
+            try {
+                System.out.print("Input message: ");
+                messageToClient = scanner.nextLine();
+                outToClient.writeBytes(messageToClient + '\n');
+                System.out.println("Send: " + messageToClient);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
